cc7cb4a9bcf067eb754c290b4f1a65de1d081cb5
MopkoBb <hqin717@gmail.com>
2017-05-16 22:49:11 +0800

    day 8

---
+++ mergeSort.py
@@ -0,0 +1,31 @@
+class Solution():
+    sum = 0
+    def MergeSort(self,lists):
+        if len(lists) <= 1:
+            print(lists)
+            return lists
+        num = int( len(lists)/2 )
+        left = self.MergeSort(lists[:num])
+        right = self.MergeSort(lists[num:])
+        print(left, right)
+        print('*****')
+        return self.Merge(left, right)
+
+    def Merge(self, left, right):
+        r, l=0, 0
+        result=[]
+        while l<len(left) and r<len(right):
+            if left[l] < right[r]:
+                result.append(left[l])
+                l += 1
+            else:
+                result.append(right[r])
+                r += 1
+                self.sum += len(left[l:])
+        result += right[r:]
+        result+= left[l:]
+        return result
+s = Solution()
+s.MergeSort([6,202,100,301,38,8,1])
+print(s.sum)
+
